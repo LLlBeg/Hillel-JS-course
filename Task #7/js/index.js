4621363dbc0b0abf1bf778ad607af26cb683e179
LLlBeg <itechnologyshved@gmail.com>
2022-07-26 16:27:45 +0300

    task #7

---
+++ Task #7/js/index.js
@@ -0,0 +1,11 @@
+import header from "./components/header.js";
+import card from "./components/card.js";
+import data from "./data.js";
+
+const root = document.querySelector("#root");
+
+root.appendChild(header("Personal Information Card"));
+
+for (let user of data) {
+  root.appendChild(card(user));
+}
