6f41ba65696b8f731aa23bc713c72a3b191fb3ac
LLlBeg <itechnologyshved@gmail.com>
2022-07-26 15:54:15 +0300

    change function

--- Task #6/main.js
+++ Task #6/main.js
@@ -1,7 +1,9 @@
 const root = document.querySelector("#root");
 
 const table = document.createElement("table");
+
 const thead = document.createElement("thead");
+
 function userHeader() {
   thead.innerHTML = `
                     <tr>
@@ -11,44 +13,47 @@ function userHeader() {
                     <th>email</th>
                     </tr>
                   `;
-
-  function UsersInformation() {
-    for (let key of users) {
-      let userRow = document.createElement("tr");
-      let headingUserId = document.createElement("td");
-      headingUserId.innerHTML = `${key.id}`;
-      let headingUserFirstName = document.createElement("td");
-      headingUserFirstName.innerHTML = `${key.first_name}`;
-      let headingUserLastName = document.createElement("td");
-      headingUserLastName.innerHTML = `${key.last_name}`;
-      let headingUserEmail = document.createElement("td");
-      headingUserEmail.innerHTML = `${key.email}`;
-
-      userRow.appendChild(headingUserId);
-      userRow.appendChild(headingUserFirstName);
-      userRow.appendChild(headingUserLastName);
-      userRow.appendChild(headingUserEmail);
-      thead.appendChild(userRow);
-
-      userRow.ondblclick = function () {
-        alert(`${key.first_name} ${key.last_name}`);
-      };
-
-      userRow.className = "boxs";
-    }
-    changeColor = function () {
-      document.querySelectorAll(".boxs").forEach((element) => {
-        element.addEventListener("click", function () {
-          this.classList.toggle("boxs-active");
-        });
-      });
-    };
-
-    changeColor();
-  }
-  table.appendChild(thead);
-  root.appendChild(table);
-  UsersInformation();
 }
 
 userHeader();
+
+function UsersInformation() {
+  for (let user of users) {
+    const userRow = document.createElement("tr");
+    let headingUserId = document.createElement("td");
+    headingUserId.innerHTML = user.id;
+    let headingUserFirstName = document.createElement("td");
+    headingUserFirstName.innerHTML = user.first_name;
+    let headingUserLastName = document.createElement("td");
+    headingUserLastName.innerHTML = user.last_name;
+    let headingUserEmail = document.createElement("td");
+    headingUserEmail.innerHTML = user.email;
+
+    userRow.appendChild(headingUserId);
+    userRow.appendChild(headingUserFirstName);
+    userRow.appendChild(headingUserLastName);
+    userRow.appendChild(headingUserEmail);
+    thead.appendChild(userRow);
+
+    userRow.ondblclick = function () {
+      alert(user.first_name + " " + user.last_name);
+    };
+
+    userRow.className = "boxs";
+  }
+
+  function changeColor() {
+    document.querySelectorAll(".boxs").forEach((e) => {
+      e.addEventListener("click", function () {
+        this.classList.toggle("boxs-active");
+      });
+    });
+  }
+
+  changeColor();
+}
+
+table.appendChild(thead);
+root.appendChild(table);
+
+UsersInformation();
